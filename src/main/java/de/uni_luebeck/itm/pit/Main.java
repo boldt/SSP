1e17f82a81e26eebd916b99cccea785a972fa276
Dennis Boldt <boldt@itm.uni-luebeck.de>
2017-04-05 16:52:05 +0200

    New SSP version

--- src/main/java/de/uni_luebeck/itm/pit/Main.java
+++ src/main/java/de/uni_luebeck/itm/pit/Main.java
@@ -4,39 +4,12 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.apache.commons.configuration.Configuration;
-
-import eu.spitfire.ssp.Initializer;
-import eu.spitfire.ssp.server.handler.cache.JenaTdbSemanticCache;
-import eu.spitfire.ssp.server.handler.cache.SemanticCache;
-
 /**
  * Created by Dennis on 2015-05-22
  */
 public class Main {
 
-	public Main() throws Exception {
-
-    	final Path dbDirectory = Paths.get("./dbDirectory");
-    	final Path spatialIndexDirectory = Paths.get("./spatialIndexDirectory");
-    	
-    	mkdir(dbDirectory);
-    	mkdir(spatialIndexDirectory);
-    	
-        Initializer initializer = new Initializer("ssp.properties") {
-
-            @Override
-            public SemanticCache createSemanticCache(Configuration config){
-            	JenaTdbSemanticCache cache = new JenaTdbSemanticCache(this.getIoExecutor(), this.getInternalTasksExecutor(), dbDirectory, spatialIndexDirectory);
-                return cache;
-            }
-
-        };
-
-        initializer.initialize();
-	}
-	
-	private void mkdir(Path p) {
+	private static void mkdir(Path p) {
     	File file = new File(p.toAbsolutePath().toString());
     	if (!file.exists()) {
     		file.mkdir();
@@ -44,7 +17,16 @@ public class Main {
 	}
 	
     public static void main(String[] args) throws Exception {
-    	new Main();
+    	
+    	System.out.println("START SSP");
+    	
+    	final Path dbDirectory = Paths.get("./dbDirectory");
+    	final Path spatialIndexDirectory = Paths.get("./ontologies");
+    	
+    	mkdir(dbDirectory);
+    	mkdir(spatialIndexDirectory);
+
+    	eu.spitfire.ssp.Main.main(new String[0]);
     }
 
 }
